ff2f247b99b3ce138ecf9a038b6a402bc5542d7b
zhengya <zhengya123>
2018-03-28 15:53:39 +0800

    “firstCommit”

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios,'8.0'
+def pods
+    pod 'CocoaAsyncSocket'
+end
+target 'socketDemo' do
+    pods
+end
--- socketDemo/ViewController.m
+++ socketDemo/ViewController.m
@@ -7,23 +7,67 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "GCDAsyncSocket.h"
+@interface ViewController ()<GCDAsyncSocketDelegate>
+@property (nonatomic, strong) GCDAsyncSocket *clientSocket;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 400, [UIScreen mainScreen].bounds.size.width, 30)];
+    btn.backgroundColor = [UIColor orangeColor];
+    [btn setTitle:@"发送数据" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(clickBtn) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+    self.clientSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    
+    NSError *error = nil;
+    [self.clientSocket connectToHost:@"127.0.0.1" onPort:12345 error:&error];
+    if (error) {
+        NSLog(@"error == %@",error);
+    }
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)clickBtn{
+    NSString *msg = @"发送数据: 你好\r\n";
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    // withTimeout -1 : 无穷大,一直等
+    // tag : 消息标记
+    [self.clientSocket writeData:data withTimeout:-1 tag:0];
 }
+
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port
+{
+    NSLog(@"链接成功");
+    NSLog(@"服务器IP: %@-------端口: %d",host,port);
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag
+{
+    NSLog(@"发送数据 tag = %zi",tag);
+    [sock readDataWithTimeout:-1 tag:tag];
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
+{
+    NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"读取数据 data = %@ tag = %zi",str,tag);
+    // 读取到服务端数据值后,能再次读取
+    [sock readDataWithTimeout:- 1 tag:tag];
+    
+}
+
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err
+{
+    NSLog(@"断开连接");
+    self.clientSocket.delegate = nil;
+    self.clientSocket = nil;
+}
+
 
 
 @end
