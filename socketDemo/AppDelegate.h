ebb82e2ea7406fe233997f8a1865ba5b869edc1b
zhengya <zhengya123>
2018-03-28 11:21:56 +0800

    Initial Commit

---
+++ socketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  socketDemo
+//
+//  Created by 中商国际 on 2018/3/28.
+//  Copyright © 2018年 中商国际. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ socketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  socketDemo
+//
+//  Created by 中商国际 on 2018/3/28.
+//  Copyright © 2018年 中商国际. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
